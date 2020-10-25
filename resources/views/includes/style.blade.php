76ba6cb0f714f35eadd3e145fbc5cff218b3d6d1
Sahabul Ashfari <sahabulashfari@gmail.com>
2020-10-25 17:11:26 +0700

    Add datatables bootstrap 4

--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -1,2 +1,2 @@
 <link rel="stylesheet" href="{{ url('style.css') }}">
-<link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="{{ url('admin/bower_components/datatables.net-bs/css/dataTables.bootstrap4.min.css') }}">
