765477f1953d597d75a85d4c7b73a2aed80e28f8
badhowi112 <baidhowi112@gmail.com>
2020-10-30 00:46:25 +0700

    update oy

--- database/seeds/EventsTableSeeder.php
+++ database/seeds/EventsTableSeeder.php
@@ -11,21 +11,8 @@ class EventsTableSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
-        \App\Events::create([
-        'name_event' => 'Race Grobokan',
-        'info_event' => 'Balapan burung sendiri',
-        'lat_event' => 3.2421321,
-        'lng_event' => -7.214213,
-        'address_event' => 'Lamongan',
-        'release_time_event' => now(),
-        'category_event' => 'Individu',
-        'price_event' => 200000
-		]);
-=======
         $sql = file_get_contents(database_path() . '/seeds/events.sql');
     
          DB::statement($sql);
->>>>>>> 86576db325defcc092e10a10958197f538df5ad2
     }
 }
