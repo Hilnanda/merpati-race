b1c631c764d35361735d5fdad6df98e55c3c11ba
Hilnanda <ardikeren75@gmail.com>
2020-11-16 21:13:27 +0700

    seeder

--- database/seeds/ClubMemberTableSeeder.php
+++ database/seeds/ClubMemberTableSeeder.php
@@ -14,33 +14,24 @@ class ClubMemberTableSeeder extends Seeder
         //
         \App\ClubMember::create([
             'id_club' => 4,
-            'id_pigeon' => 1,
+            'id_user' => 3,
             'is_active' => 1
             ]);
         \App\ClubMember::create([
             'id_club' => 5,
-            'id_pigeon' => 2,
+            'id_user' => 4,
             'is_active' => 1
             ]);
         \App\ClubMember::create([
             'id_club' => 6,
-            'id_pigeon' => 3,
+            'id_user' => 5,
             'is_active' => 1
              ]);
         \App\ClubMember::create([
-            'id_club' => 3,
-            'id_pigeon' => 4,
+            'id_club' => 6,
+            'id_user' => 6,
             'is_active' => 1
              ]);
-        \App\ClubMember::create([
-            'id_club' => 3,
-            'id_pigeon' => 5,
-            'is_active' => 1
-             ]);
-        \App\ClubMember::create([
-            'id_club' => 4,
-            'id_pigeon' => 6,
-            'is_active' => 1
-             ]);
+        
     }
 }
