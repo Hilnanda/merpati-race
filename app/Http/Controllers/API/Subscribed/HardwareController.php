2bc709617c480bd2c0ddb26f0333166ee7efc5c5
Sahabul Ashfari <sahabulashfari@gmail.com>
2020-12-08 04:54:44 +0700

    Add api for hardware

---
+++ app/Http/Controllers/API/Subscribed/HardwareController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers\API\Subscribed;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Loft;
+use App\LoftMember;
+use App\User;
+use App\Pigeons;
+use App\Events;
+use App\EventParticipants;
+use App\EventResults;
+use App\EventHotspot;
+
+class HardwareController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Get message from hardware.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function sendMessage($message)
+    {
+        $event = Events::first();
+
+        $input['address_event'] = $message;
+
+        $event->update($input);
+
+        return response()->json(Events::find($event->id));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
