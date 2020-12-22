b6402fca106272d8b1d55f6b76ab6c05b1d14f62
Hilnanda <ardikeren75@gmail.com>
2020-12-23 01:40:26 +0700

    timer

--- resources/views/admin/pages/list-pigeon.blade.php
+++ resources/views/admin/pages/list-pigeon.blade.php
@@ -216,4 +216,10 @@
       })
     })
   </script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.3.0/jquery.min.js" type="text/javascript"></script>
+  <script type="text/javascript">
+ window.setTimeout(function () {
+  window.location.reload();
+}, 20000);
+  </script>
 @endpush
